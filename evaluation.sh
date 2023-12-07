ca3a44365c57772a343a968579d8a97882410168
Enrique <enriquec@mit.edu>
2023-12-07 13:24:44 -0500

    Uploaded the evaluation images

---
+++ evaluation.sh
@@ -0,0 +1,27 @@
+# python3 scripts/run_exps.py --name=bba-livestream --trace_set=hsdpa --n_train_runs=8 --n_test_runs=8 \
+#  --results_dir=results/ --rt --target_thr=5.0 -- --video real/data/videos/livestream/video.dat --max-chunks=-1 --live-delay=8
+
+#  python3 scripts/run_exps.py --name=tb-livestream --trace_set=hsdpa --n_train_runs=8 --n_test_runs=8 \
+#  --results_dir=results/ --tb --target_thr=5.0 -- --video real/data/videos/livestream/video.dat --max-chunks=-1 --live-delay=8
+
+#  python3 scripts/run_exps.py --name=bola-livestream --trace_set=hsdpa --n_train_runs=8 --n_test_runs=8 \
+#  --results_dir=results/ --bola --target_thr=5.0 -- --video real/data/videos/livestream/video.dat --max-chunks=-1 --live-delay=8
+
+
+#  python3 scripts/run_exps.py --name=bba-livestream-single --trace_set=hsdpa --n_train_runs=8 --n_test_runs=8 \
+#  --results_dir=results/ --rt --target_thr=5.0 -- --video real/data/videos/livestream/video.dat --max-chunks=-1 --live-delay=8 --single-quality
+
+#  python3 scripts/run_exps.py --name=tb-livestream-single --trace_set=hsdpa --n_train_runs=8 --n_test_runs=8 \
+#  --results_dir=results/ --tb --target_thr=5.0 -- --video real/data/videos/livestream/video.dat --max-chunks=-1 --live-delay=8 --single-quality
+
+#  python3 scripts/run_exps.py --name=bola-livestream-single --trace_set=hsdpa --n_train_runs=8 --n_test_runs=8 \
+#  --results_dir=results/ --bola --target_thr=5.0 -- --video real/data/videos/livestream/video.dat --max-chunks=-1 --live-delay=8 --single-quality
+
+python3 scripts/run_exps.py --name=bba-livestream-delay-4 --trace_set=hsdpa --n_train_runs=8 --n_test_runs=8 \
+ --results_dir=results/ --rt --target_thr=5.0 -- --video real/data/videos/livestream/video.dat --max-chunks=-1 --live-delay=4
+
+ python3 scripts/run_exps.py --name=tb-livestream-delay-4 --trace_set=hsdpa --n_train_runs=8 --n_test_runs=8 \
+ --results_dir=results/ --tb --target_thr=5.0 -- --video real/data/videos/livestream/video.dat --max-chunks=-1 --live-delay=4
+
+ python3 scripts/run_exps.py --name=bola-livestream-delay-4 --trace_set=hsdpa --n_train_runs=8 --n_test_runs=8 \
+ --results_dir=results/ --bola --target_thr=5.0 -- --video real/data/videos/livestream/video.dat --max-chunks=-1 --live-delay=4
